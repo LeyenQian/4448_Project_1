e5d2abf20d3c852c575c4c9b8b508ac635a2dcdd
Leyen <leyenqian@gmail.com>
2020-09-16 13:27:04 -0600

    add two override methods to wolf class

--- Animals/Canine/Breed/Wolf.java
+++ Animals/Canine/Breed/Wolf.java
@@ -8,4 +8,16 @@ public class Wolf extends Canine
     {
         super(name);
     }
+
+    // override the wake_up and eat methods
+    // this is for project requirement
+    public void wake_up()
+    {
+        System.out.println(String.format("#[%s] %s wakes up (overrided).", this.type, this.name));
+    }
+
+    public void eat()
+    {
+        System.out.println(String.format("#[%s] %s eats. (overrided)", this.type, this.name));
+    }
 }
